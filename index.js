4e2af2ce1c9979e81c0653de4a2a584579e8c3dc
karuirujackson <jkjackaruiru641@gmail.com>
2023-12-06 13:48:55 +0300

    Recursive vs Iterative Traversal in LinkedLists

---
+++ index.js
@@ -0,0 +1,15 @@
+const Node = require('./Node');
+const LinkedList = require('./LinkedList');
+
+const myList = new LinkedList();
+
+myList.addToHead('Node 1');
+myList.addToHead('Node 2');
+myList.addToHead('Node 3');
+myList.addToHead('Node 4');
+
+const foundNode = myList.findNodeIteratively('Node 3');
+console.log(foundNode);
+
+const myNodeRecursive = myList.findNodeRecursively('Node 3');
+console.log(myNodeRecursive);
